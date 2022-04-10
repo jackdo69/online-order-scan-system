ac46c2175d50c50f79297a5498674b3498893d6a
jackdo69 <jackie.duc.do@gmail.com>
2022-04-10 18:22:02 +1000

    create basic models

---
+++ backend/models/order.model.ts
@@ -0,0 +1,22 @@
+import { Customer } from "./customer.model";
+import { Product } from "./product.model";
+
+export interface Item extends Product {
+  quantity: number;
+}
+
+export enum Status {
+  CREATED = "created",
+  SCANNED = "scanned",
+  APPROVED = "approved",
+  DECLINED = "declined"
+}
+
+export interface Order {
+  items: Item[];
+  code: string;
+  status: Status;
+  customer: Customer;
+  created: string;
+  updated: string;
+}
