905587a958a7c57c32fd2e1fd1cbf241ca881885
jackdo69 <jackie.duc.do@gmail.com>
2022-04-11 23:15:34 +1000

    adding dynamo

--- cdk/lib/online-order-scan-system-stack.ts
+++ cdk/lib/online-order-scan-system-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { createLambda } from "./helper";
 import { ApiGateway } from "./ApiGateway";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class OnlineOrderScanSystemStack extends Stack {
@@ -14,5 +15,23 @@ export class OnlineOrderScanSystemStack extends Stack {
     const api = new ApiGateway(this, "apigateway");
 
     api.addIntegration("GET", "/health", health);
+
+    // Create dynamo table
+    const orderSystemTable = new Table(this, "order-system-db", {
+      partitionKey: { name: "composite_id", type: AttributeType.STRING },
+      sortKey: { name: "sort_key", type: AttributeType.STRING },
+      tableName: "order-system-db"
+    });
+    orderSystemTable.addGlobalSecondaryIndex({
+      indexName: "order-system-db-sort_key-composite_id-index",
+      partitionKey: {
+        name: "sort_key",
+        type: AttributeType.STRING
+      },
+      sortKey: {
+        name: "composite_id",
+        type: AttributeType.STRING
+      }
+    });
   }
 }
