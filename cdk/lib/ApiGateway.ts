9fdfba3f376d5009b2db25be2c432b4e5cc4a29d
jackdo69 <jackie.duc.do@gmail.com>
2022-04-19 22:42:58 +1000

    add log group for api gateway

--- cdk/lib/ApiGateway.ts
+++ cdk/lib/ApiGateway.ts
@@ -1,8 +1,19 @@
-import { AuthorizationType, CognitoUserPoolsAuthorizer, LambdaIntegration, MethodOptions, RestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+  AccessLogFormat,
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  LambdaIntegration,
+  LogGroupLogDestination,
+  MethodOptions,
+  RestApi,
+  AccessLogField as al
+} from "aws-cdk-lib/aws-apigateway";
 import { IFunction } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { getBranchName } from "./helper";
 import { Cognito } from "./Cognito";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
+import { RemovalPolicy } from "aws-cdk-lib";
 
 export class ApiGateway extends RestApi {
   public static readonly ID = "Apigateway";
@@ -10,7 +21,24 @@ export class ApiGateway extends RestApi {
 
   constructor(private scope: Construct, apiName: string) {
     super(scope, "ApiGateway", {
-      restApiName: `orderSystem_${getBranchName(scope)}`
+      restApiName: `orderSystem_${getBranchName(scope)}`,
+      deployOptions: {
+        accessLogDestination: new LogGroupLogDestination(
+          new LogGroup(scope, `${apiName}`, {
+            logGroupName: "apiGatewayLogGroup",
+            retention: RetentionDays.ONE_WEEK,
+            removalPolicy: RemovalPolicy.DESTROY
+          })
+        ),
+        metricsEnabled: true,
+        accessLogFormat: AccessLogFormat.custom(
+          `${al.contextIdentitySourceIp} ${al.contextIdentityCaller()} ${al.contextIdentityUser()} [${
+            al.contextRequestTime
+          }]${al.contextHttpMethod()} ${al.contextResourcePath} ${al.contextProtocol} ${al.contextStatus} ${al.contextResponseLength} ${
+            al.contextRequestTimeEpoch
+          }${al.contextRequestId()} `
+        )
+      }
     });
     const auth = new Cognito(scope);
     this._authorizer = auth.authorizer;
